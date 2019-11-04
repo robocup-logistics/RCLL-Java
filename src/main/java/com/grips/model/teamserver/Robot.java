856205ac5425bd5dc21a3a42c6be9cc36fb5afda
Lukas Knoflach <knoflach@student.tugraz.at>
2019-11-04 19:17:44 +0100

    new maven version

---
+++ src/main/java/com/grips/model/teamserver/Robot.java
@@ -0,0 +1,110 @@
+/*
+ *
+ * Copyright (c) 2017, Graz Robust and Intelligent Production System (grips)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package com.grips.model.teamserver;
+
+import java.net.Socket;
+
+public class Robot {
+
+    public int hashCode() {
+        return new Long(id).intValue();
+    }
+
+    public int getWaitingTime() {
+        return waitingTime;
+    }
+
+    public void setWaitingTime(int waitingTime) {
+        this.waitingTime = waitingTime;
+    }
+
+    public enum RobotState {
+        ACTIVE,
+        MAINTENANCE,
+        DISQUALIFIED
+    }
+
+    private long id;
+
+    private boolean waiting;
+
+    private long lastActive;
+
+    private long timeLastTaskAssignment;
+
+    private RobotState robotState;
+
+    private int waitingTime;
+
+    private Socket connection;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public boolean isWaiting() {
+        return waiting;
+    }
+
+    public void setWaiting(boolean waiting) {
+        this.waiting = waiting;
+    }
+
+
+    public RobotState getRobotState() { return robotState; }
+
+    public void setRobotState(RobotState robotState) { this.robotState = robotState; }
+
+    public boolean equals(Object other) {
+        if (other == null) return false;
+        if (other == this) return true;
+        if (!(other instanceof Robot)) return false;
+        Robot otherRobot = (Robot) other;
+        if (this.getId() != otherRobot.getId()) return false;
+        return true;
+    }
+
+    public long getLastActive() {
+        return lastActive;
+    }
+
+    public void setLastActive(long lastActive) {
+        this.lastActive = lastActive;
+    }
+
+    public Socket getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Socket connection) {
+        this.connection = connection;
+    }
+
+    public long getTimeLastTaskAssignment() {
+        return timeLastTaskAssignment;
+    }
+
+    public void setTimeLastTaskAssignment(long timeLastTaskAssignment) {
+        this.timeLastTaskAssignment = timeLastTaskAssignment;
+    }
+}
+
---
+++ src/main/java/org/robocup_logistics/llsf_comm/ProtobufMessageHandler.java
@@ -0,0 +1,34 @@
+package org.robocup_logistics.llsf_comm;
+
+import com.google.protobuf.GeneratedMessage;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+/**
+ * The Interface ProtobufMessageHandler is used to handle received protobuf messages. Implement
+ * this interface if you want to be able to access the information in the retrieved messages.
+ * The ProtobufClient/ProtobufBroadcastPeer automatically passes incoming messages to your handler
+ * if you have registered it.
+ * 
+ * @see ProtobufClient#register_handler(ProtobufMessageHandler)
+ * @see ProtobufBroadcastPeer#register_handler(ProtobufMessageHandler handler)
+ */
+public interface ProtobufMessageHandler {
+	
+	/**
+	 * This method is called by the ProtobufClient/ProtobufBroadcastPeer. The GeneratedMessage
+	 * passed to it is an instance of the same type as the protobuf message you received. It is
+	 * used to identify the type of the protobuf message. The actual model is contained in the
+	 * ByteBuffer. You can read the tutorial to find out how to handle incoming messages correctly.
+	 *  
+	 * @param in_msg
+	 *            the ByteBuffer containing the actual model
+	 * @param msg
+	 *            the instance of the same type as the protobuf message you received
+	 */
+	public void handle_message(ByteBuffer in_msg, GeneratedMessage msg);
+	public void connection_lost(IOException e);
+	public void timeout();
+
+}
