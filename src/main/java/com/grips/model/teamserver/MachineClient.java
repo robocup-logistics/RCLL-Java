b178d5f5d49a32a6e5cfcfdbd157c273e9eec5ac
Stefan Moser <stefan.moser@student.tugraz.at>
2020-01-20 17:04:39 +0100

    migrated sendprepare functions

---
+++ src/main/java/com/grips/model/teamserver/MachineClient.java
@@ -0,0 +1,121 @@
+package com.grips.model.teamserver;
+
+import com.google.protobuf.GeneratedMessage;
+import org.robocup_logistics.llsf_msgs.MachineDescriptionProtos;
+import org.robocup_logistics.llsf_msgs.MachineInstructionProtos;
+import org.robocup_logistics.llsf_msgs.ProductColorProtos;
+import org.robocup_logistics.llsf_msgs.TeamProtos;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MachineClient {
+
+    private MachineClientUtils.TeamColor color;
+    private Map<MachineClientUtils.Machine, GeneratedMessage> sendQueue;
+
+    public MachineClient (MachineClientUtils.TeamColor color)
+    {
+        this.color = color;
+        this.sendQueue = new ConcurrentHashMap<>();
+    }
+
+    private void addMessageToSendQueue(MachineClientUtils.Machine machine, GeneratedMessage msg)
+    {
+        sendQueue.put(machine, msg);
+    }
+
+    public void sendResetMachine(MachineClientUtils.Machine machine)
+    {
+        MachineInstructionProtos.ResetMachine reset = MachineInstructionProtos.ResetMachine.newBuilder()
+                .setMachine(machine.toString())
+                .setTeamColor(TeamProtos.Team.valueOf(color.toString()))
+                .build();
+        addMessageToSendQueue(machine, reset);
+    }
+
+    private void sendPrepareBS(MachineClientUtils.MachineSide side, MachineClientUtils.BaseColor base_color)
+    {
+        MachineClientUtils.Machine machine = MachineClientUtils.Machine.BS;
+        MachineInstructionProtos.PrepareInstructionBS bsInstruction =
+                MachineInstructionProtos.PrepareInstructionBS.newBuilder()
+                        //.setSide(task.getSide() == SubProductionTask.MachineSide.INPUT ? MachineDescriptionProtos.MachineSide.INPUT : MachineDescriptionProtos.MachineSide.OUTPUT)
+                        .setSide(side == MachineClientUtils.MachineSide.Input ? MachineInstructionProtos.MachineSide.INPUT : MachineInstructionProtos.MachineSide.OUTPUT)
+                        .setColor(ProductColorProtos.BaseColor.valueOf(base_color.toString()))
+                        .build();
+        MachineInstructionProtos.PrepareMachine prepareMachineMsg =
+                MachineInstructionProtos.PrepareMachine.newBuilder()
+                        .setTeamColor(TeamProtos.Team.valueOf(color.toString()))
+                        .setMachine(machine.toString())
+                        .setInstructionBs(bsInstruction)
+                        .build();
+
+        addMessageToSendQueue(machine, prepareMachineMsg);
+    }
+
+    private void sendPrepareDS(int gate, int orderId) {
+        MachineClientUtils.Machine machine = MachineClientUtils.Machine.DS;
+        MachineInstructionProtos.PrepareInstructionDS dsInstruction =
+                MachineInstructionProtos.PrepareInstructionDS.newBuilder()
+                        .setGate(gate)
+                        .setOrderId(orderId)
+                        .build();
+        MachineInstructionProtos.PrepareMachine prepareMachineMsg =
+                MachineInstructionProtos.PrepareMachine.newBuilder()
+                        .setTeamColor(TeamProtos.Team.valueOf(color.toString()))
+                        .setMachine(machine.toString())
+                        .setInstructionDs(dsInstruction)
+                        .build();
+
+        addMessageToSendQueue(machine, prepareMachineMsg);
+    }
+
+    private void sendPrepareRS(MachineClientUtils.Machine machine, MachineClientUtils.RingColor color) {
+        ProductColorProtos.RingColor refbox_color = ProductColorProtos.RingColor.valueOf(color.ordinal()+1);
+        MachineInstructionProtos.PrepareInstructionRS rsInstruction =
+                MachineInstructionProtos.PrepareInstructionRS.newBuilder()
+                        .setRingColor(refbox_color)
+                        .build();
+
+        MachineInstructionProtos.PrepareMachine prepareMachineMsg =
+                MachineInstructionProtos.PrepareMachine.newBuilder()
+                        .setTeamColor(TeamProtos.Team.valueOf(color.toString()))
+                        .setMachine(machine.toString())
+                        .setInstructionRs(rsInstruction)
+                        .build();
+
+        addMessageToSendQueue(machine, prepareMachineMsg);
+    }
+
+    private void sendPrepareCS(MachineClientUtils.Machine machine, MachineClientUtils.CSOp operation) {
+        MachineDescriptionProtos.CSOp refbox_operation = MachineDescriptionProtos.CSOp.valueOf(operation.ordinal() + 1);
+        MachineInstructionProtos.PrepareInstructionCS csInstruction =
+                MachineInstructionProtos.PrepareInstructionCS.newBuilder()
+                        .setOperation(refbox_operation)
+                        .build();
+
+        MachineInstructionProtos.PrepareMachine prepareMachineMsg =
+                MachineInstructionProtos.PrepareMachine.newBuilder()
+                        .setTeamColor(TeamProtos.Team.valueOf(color.toString()))
+                        .setMachine(machine.toString())
+                        .setInstructionCs(csInstruction)
+                        .build();
+
+        addMessageToSendQueue(machine, prepareMachineMsg);
+    }
+
+    private void sendPrepareSS(MachineClientUtils.Machine machine, MachineInstructionProtos.SSTask ssTask) {
+
+        MachineInstructionProtos.PrepareInstructionSS ssInstruction = MachineInstructionProtos.PrepareInstructionSS.newBuilder()
+                .setTask(ssTask)
+                .build();
+
+        MachineInstructionProtos.PrepareMachine prepareMachineMsg = MachineInstructionProtos.PrepareMachine.newBuilder()
+                .setTeamColor(TeamProtos.Team.valueOf(color.toString()))
+                .setMachine(machine.toString())
+                .setInstructionSs(ssInstruction)
+                .build();
+
+        addMessageToSendQueue(machine, prepareMachineMsg);
+    }
+}
---
+++ src/main/java/com/grips/model/teamserver/MachineClientUtils.java
@@ -0,0 +1,10 @@
+package com.grips.model.teamserver;
+
+public class MachineClientUtils {
+    public enum TeamColor {cyan, magenta}
+    public enum Machine {BS, DS, RS_1, RS_2, CS_1, CS_2, SS}
+    public enum MachineSide {Input, Output}
+    public enum BaseColor {BASE_RED, BASE_BLACK, BASE_SILVER}
+    public enum RingColor {RING_BLUE, RING_GREEN, RING_ORANGE, RING_YELLOW}
+    public enum CSOp {RETRIEVE_CAP, MOUNT_CAP}
+}
