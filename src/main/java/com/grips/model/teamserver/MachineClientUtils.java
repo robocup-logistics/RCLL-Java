a985fa76627d96bfae8624e044e2e18fbdc0151c
Stefan Moser <stefan.moser@student.tugraz.at>
2020-01-27 21:00:59 +0100

    changed value of teamcolor enum

--- src/main/java/com/grips/model/teamserver/MachineClientUtils.java
+++ src/main/java/com/grips/model/teamserver/MachineClientUtils.java
@@ -1,7 +1,7 @@
 package com.grips.model.teamserver;
 
 public class MachineClientUtils {
-    public enum TeamColor {cyan, magenta}
+    public enum TeamColor {CYAN, MAGENTA}
     public enum Machine {BS, DS, RS_1, RS_2, CS_1, CS_2, SS}
     public enum MachineSide {Input, Output}
     public enum BaseColor {BASE_RED, BASE_BLACK, BASE_SILVER}
