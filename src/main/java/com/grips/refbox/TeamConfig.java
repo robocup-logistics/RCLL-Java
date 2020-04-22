24cbfa7f6f5f564b9cf5045d667074e3df1aadfa
pkohout <peter.kohout@student.tugraz.at>
2020-04-23 00:23:39 +0200

    add constructors to configs.

--- src/main/java/com/grips/refbox/TeamConfig.java
+++ src/main/java/com/grips/refbox/TeamConfig.java
@@ -1,10 +1,14 @@
 package com.grips.refbox;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TeamConfig {
     private String cryptoKey;
     private String color;
