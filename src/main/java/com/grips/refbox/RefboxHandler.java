d686fddc48ebe226bc76a455d1398262439fdd0e
pkohout <peter.kohout@student.tugraz.at>
2020-04-22 23:18:34 +0200

    fix problems NPEs in RefboxHandler

--- src/main/java/com/grips/refbox/RefboxHandler.java
+++ src/main/java/com/grips/refbox/RefboxHandler.java
@@ -2,6 +2,10 @@ package com.grips.refbox;
 
 import com.google.protobuf.GeneratedMessageV3;
 import com.google.protobuf.InvalidProtocolBufferException;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.extern.apachecommons.CommonsLog;
+import org.robocup_logistics.llsf_comm.ProtobufMessageHandler;
 import org.robocup_logistics.llsf_msgs.*;
 
 import java.io.BufferedOutputStream;
@@ -9,7 +13,9 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.function.Consumer;
 
-public class RefboxHandler {
+@Setter
+@CommonsLog
+public class RefboxHandler implements ProtobufMessageHandler {
     private Consumer<MachineInfoProtos.MachineInfo> consumer_machine_info;
     private Consumer<GameStateProtos.GameState> consumer_game_state;
     private Consumer<ExplorationInfoProtos.ExplorationInfo> consumer_exploration_info;
@@ -20,158 +26,67 @@ public class RefboxHandler {
     private Consumer<OrderInfoProtos.OrderInfo> consumer_order_info;
     private Consumer<RingInfoProtos.RingInfo> consumer_ring_info;
 
-    public void RegisterHandleMachineInfo(Consumer<MachineInfoProtos.MachineInfo> machine_info)
-    {
-        this.consumer_machine_info = machine_info;
+    private RefboxHandler() {
+        //setting default handler which only log a message that nothing will be don
+        consumer_machine_info = (msg) -> log.debug("consumer_machine_info not set");
+        consumer_game_state = (msg) -> log.debug("consumer_game_state not set");
+        consumer_exploration_info = (msg) -> log.debug("consumer_exploration_info not set");
+        consumer_version_info = (msg) -> log.debug("consumer_version_info not set");
+        consumer_robot_info = (msg) -> log.debug("consumer_robot_info not set");
+        consumer_machine_report = (msg) -> log.debug("consumer_machine_report not set");
+        consumer_beacon_sig = (msg) -> log.debug("consumer_beacon_sig not set");
+        consumer_order_info = (msg) -> log.debug("consumer_order_info not set");
+        consumer_ring_info = (msg) -> log.debug("consumer_ring_info not set");
     }
 
-    public void RegisterHandleGameState (Consumer<GameStateProtos.GameState> game_state)
-    {
-        this.consumer_game_state = game_state;
-    }
-
-    public void RegisterHandleExplorationInfo (Consumer<ExplorationInfoProtos.ExplorationInfo> exploration_info)
-    {
-        this.consumer_exploration_info = exploration_info;
-    }
-
-    public void RegisterHandleVersionInfo (Consumer<VersionProtos.VersionInfo> version_info)
-    {
-        this.consumer_version_info = version_info;
-    }
-
-    public void RegisterHandleRobotInfo (Consumer<RobotInfoProtos.RobotInfo> robot_info)
-    {
-        this.consumer_robot_info = robot_info;
-    }
-
-    public void RegisterHandleMachineReport (Consumer<MachineReportProtos.MachineReportInfo> machine_report)
-    {
-        this.consumer_machine_report = machine_report;
-    }
-
-    public void RegisterHandleBeaconSignal (Consumer<BeaconSignalProtos.BeaconSignal> beacon_sig)
-    {
-        this.consumer_beacon_sig = beacon_sig;
-    }
-
-    public void RegisterHandleOrderInfo (Consumer<OrderInfoProtos.OrderInfo> order_info)
-    {
-        this.consumer_order_info = order_info;
-    }
-
-    public void RegisterHandleRingInfo (Consumer<RingInfoProtos.RingInfo> ring_info)
-    {
-        this.consumer_ring_info = ring_info;
-    }
-
-    public void handle_message(ByteBuffer in_msg, GeneratedMessageV3 msg) {
-        if (null == in_msg) {
-            System.err.println("ERROR: in_msg is null in TeamHandler!");
-            return;
-        }
-        if (null == msg) {
-            System.err.println("ERROR: msg is null in TeamHandler!");
-            return;
-        }
-
+    @Override
+    public void handle_message(@NonNull ByteBuffer in_msg, @NonNull GeneratedMessageV3 msg) {
         byte[] array = new byte[in_msg.capacity()];
         in_msg.rewind();
         in_msg.get(array);
 
+        //todo remove this!
         try {
             msg.writeTo(new BufferedOutputStream(System.out));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        if (msg instanceof BeaconSignalProtos.BeaconSignal) {
-            BeaconSignalProtos.BeaconSignal beacon_signal;
-            try {
-                beacon_signal = BeaconSignalProtos.BeaconSignal.parseFrom(array);
-                //handleBeaconSignal(beacon_signal);
-                consumer_beacon_sig.accept(beacon_signal);
-            } catch (InvalidProtocolBufferException e) {
-                e.printStackTrace();
+        try {
+            if (msg instanceof BeaconSignalProtos.BeaconSignal) {
+                consumer_beacon_sig.accept(BeaconSignalProtos.BeaconSignal.parseFrom(array));
+            } else if (msg instanceof OrderInfoProtos.OrderInfo) {
+                consumer_order_info.accept(OrderInfoProtos.OrderInfo.parseFrom(array));
+            } else if (msg instanceof RingInfoProtos.RingInfo) {
+                consumer_ring_info.accept(RingInfoProtos.RingInfo.parseFrom(array));
+            } else if (msg instanceof MachineInfoProtos.MachineInfo) {
+                consumer_machine_info.accept(MachineInfoProtos.MachineInfo.parseFrom(array));
+            } else if (msg instanceof MachineReportProtos.MachineReportInfo) {
+                consumer_machine_report.accept(MachineReportProtos.MachineReportInfo.parseFrom(array));
+            } else if (msg instanceof GameStateProtos.GameState) {
+                consumer_game_state.accept(GameStateProtos.GameState.parseFrom(array));
+            } else if (msg instanceof ExplorationInfoProtos.ExplorationInfo) {
+                consumer_exploration_info.accept(ExplorationInfoProtos.ExplorationInfo.parseFrom(array));
+            } else if (msg instanceof VersionProtos.VersionInfo) {
+                consumer_version_info.accept(VersionProtos.VersionInfo.parseFrom(array));
+            } else if (msg instanceof RobotInfoProtos.RobotInfo) {
+                consumer_robot_info.accept(RobotInfoProtos.RobotInfo.parseFrom(array));
+            } else {
+                log.error(this.getClass() + " received unkown msg!");
             }
+        } catch (InvalidProtocolBufferException e) {
+            log.error("Invalid ProtoBuf Message!", e);
         }
-        else if (msg instanceof OrderInfoProtos.OrderInfo) {
-            OrderInfoProtos.OrderInfo order_info;
-            try {
-                System.out.println("received private order !!!!!!!!!");
-                order_info = OrderInfoProtos.OrderInfo.parseFrom(array);
-                //handleOrderInfo(order_info);
-                consumer_order_info.accept(order_info);
+    }
 
-            } catch (InvalidProtocolBufferException e) {
-                e.printStackTrace();
-            }
-        } else if (msg instanceof RingInfoProtos.RingInfo) {
-            RingInfoProtos.RingInfo ring_info;
-            try {
-                ring_info = RingInfoProtos.RingInfo.parseFrom(array);
-                //handleRingInfo(ring_info);
-                consumer_ring_info.accept(ring_info);
-            } catch (InvalidProtocolBufferException e) {
-                e.printStackTrace();
-            }
-        } else if (msg instanceof MachineInfoProtos.MachineInfo) {
-            MachineInfoProtos.MachineInfo machine_info;
-            try {
-                machine_info = MachineInfoProtos.MachineInfo.parseFrom(array);
-                //handleMachineInfo(machine_info);
-                consumer_machine_info.accept(machine_info);
-            } catch (InvalidProtocolBufferException e) {
-                e.printStackTrace();
-            }
-        } else if (msg instanceof MachineReportProtos.MachineReportInfo) {
-            MachineReportProtos.MachineReportInfo machine_report_info;
-            try {
-                machine_report_info = MachineReportProtos.MachineReportInfo.parseFrom(array);
-                //handleMachineReport(machine_report_info);
-                consumer_machine_report.accept(machine_report_info);
-            } catch (InvalidProtocolBufferException e) {
-                e.printStackTrace();
-            }
-        } else if (msg instanceof GameStateProtos.GameState) {
-            GameStateProtos.GameState game_state;
-            try {
-                game_state = GameStateProtos.GameState.parseFrom(array);
-                //handleGameState(game_state);
-                consumer_game_state.accept(game_state);
-            } catch (InvalidProtocolBufferException e) {
-                e.printStackTrace();
-            }
-        } else if (msg instanceof ExplorationInfoProtos.ExplorationInfo) {
-            ExplorationInfoProtos.ExplorationInfo exploration_info;
-            try {
-                exploration_info = ExplorationInfoProtos.ExplorationInfo.parseFrom(array);
-                //handleExplorationInfo(exploration_info);
-                consumer_exploration_info.accept(exploration_info);
-            } catch (InvalidProtocolBufferException e) {
-                System.out.println(e.getMessage());
-                e.printStackTrace();
-            }
-        } else if (msg instanceof VersionProtos.VersionInfo) {
-            VersionProtos.VersionInfo version_info;
-            try {
-                version_info = VersionProtos.VersionInfo.parseFrom(array);
-                //handleVersionInfo(version_info);
-                consumer_version_info.accept(version_info);
-            } catch (InvalidProtocolBufferException e) {
-                e.printStackTrace();
-            }
-        } else if (msg instanceof RobotInfoProtos.RobotInfo) {
-            RobotInfoProtos.RobotInfo robot_info;
-            try {
-                robot_info = RobotInfoProtos.RobotInfo.parseFrom(array);
-                //handleRobotInfo(robot_info);
-                consumer_robot_info.accept(robot_info);
-            } catch (InvalidProtocolBufferException e) {
-                e.printStackTrace();
-            }
-        } else {
-            System.err.println("ERROR: TeamHandler received unkown msg!");
-        }
+    @Override
+    public void connection_lost(IOException e) {
+        log.error(this.getClass() + " lost connection to the RefBox!", e);
+    }
+
+
+    @Override
+    public void timeout() {
+        log.warn(this.getClass() + " timeout in connection to RefBox at channel Public!");
     }
 }
